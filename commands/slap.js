fd31e62ecea862d1581c89f2d64a58de468080d2
NotMoldyxD <71900226+NotMoldyxD@users.noreply.github.com>
2020-09-25 15:47:36 -0400

    Update slap.js

--- commands/slap.js
+++ commands/slap.js
@@ -1,16 +1,11 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const { MessageEmbed } = require('discord.js');
+const config = require('../config.json');
 
 module.exports = {
     name: "slap",
-    description: "slaps a member",
-    category: "Action",
-    cooldown: 3,
-    usage: "<@Name>",
-    execute(message) {
-        const mentionedMember = message.mentions.members.first()
-
-        const gifs = message.content.slice(prefix.length+6)
+    category: "actions",
+    run: async (client, message, args) => {
+        const gifs = message.content.slice(config.prefix.length+6)
 
         if (!gifs) {
             return message.channel.send("**You need to tell me who you would like to slaP!**")
@@ -19,31 +14,11 @@ module.exports = {
                 "https://media.giphy.com/media/vxvNnIYFcYqEE/source.gif"
             ]
 
-            let reponses = [
-                `${message.author.username} just slapped ${mentionedMember.user.username}! `
-            ]
+        const embed = new MessageEmbed()
+            .setTitle(`@${message.author.username} slaps @${message.mentions.users.first().username || message.mentions.members.first()}`)
+            .setImage(gifs.link)
 
-            let botMessgaes = [
-                `**${message.author.username} You cannot slap yourself!**`
-            ]
-
-            const randomNumber = Math.floor(Math.random() * gifs.length)
-            const randomGif = gifs[randomNumber]
-
-            const randomNumber2 = Math.floor(Math.random() * reponses.length)
-            const randomResponse = reponses[randomNumber2]
-
-            const randomNumber3 = Math.floor(Math.random() * botMessgaes.length)
-            const randomBotMsg = botMessgaes[randomNumber3]
-
-            if (mentionedMember.user.id === message.author.id) return message.channel.send(randomBotMsg)
-
-            var embed = new Discord.MessageEmbed()
-            .setAuthor(randomResponse, message.author.displayAvatarURL())
-            .setColor('YELLOW')
-            .setImage(randomGif)
-            .setTimestamp()
-            message.channel.send(embed)
+        await message.channel.send(embed)
         }
     }
 }
