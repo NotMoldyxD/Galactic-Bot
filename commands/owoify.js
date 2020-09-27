9c73f3dae1987273abdc9363e0400eb78bd4fb18
NotMoldyxD <71900226+NotMoldyxD@users.noreply.github.com>
2020-09-26 23:37:06 -0400

    Create owoify.js

---
+++ commands/owoify.js
@@ -0,0 +1,18 @@
+  
+const Discord = require('discord.js');
+const superagent = require('superagent');
+const customisation = require('../customisation.json');
+module.exports = {
+  name: 'owoify',
+  description: 'OwO-ify a message',
+  usage: 'owoify',
+  category: 'fun',
+run: async (client, message, args, tools) => {
+    if (!args[0]) return message.reply("You need to input a sentence to OwOify")
+    const { body } = await superagent
+    .get("https://nekos.life/api/v2/owoify?text=" + args.join('%20'));
+    
+    
+    message.channel.send(body.owo)
+}
+};
