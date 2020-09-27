4bb7ea29642b93f1118fd51b2adcfd4f6f056a8a
NotMoldyxD <71900226+NotMoldyxD@users.noreply.github.com>
2020-09-27 13:42:22 -0400

    Create coinflip.js

---
+++ commands/coinflip.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: 'coinflip',
+    description: 'Flip a Coin',
+    usage: 'coinflip',
+    category 'fun',
+    run: async (bot, message, args) => {
+    let random = (Math.floor(Math.random() * Math.floor(2)));
+    if(random === 0) {
+      message.channel.send('Heads!');
+    }
+    else {
+      message.channel.send('Tails!');
+    }
+  }
+},
+
