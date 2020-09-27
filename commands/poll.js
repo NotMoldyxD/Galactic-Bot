60136733d12a2621b2d1e143eb5f102ab3941f87
NotMoldyxD <71900226+NotMoldyxD@users.noreply.github.com>
2020-09-27 12:45:36 -0400

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -4,30 +4,17 @@ module.exports = {
   description: "Create a simple yes or no poll",
   category: "fun",
   run: async (client, message, args) => {
-    if (!message.member.permissions.has("ADMINISTRATOR"))
-      return message.channel.send(
-        `You do not have admin, ${message.author.username}`
-      );
-    const channel =
-      message.mentions.channels.first() ||
-      message.guild.channels.cache.get(args[0]);
-    if (!channel) {
-      return message.channel.send(
-        `You did not mention / give the id of your channel!`
-      );
+        let pollChannel = message.mentions.channels.first();
+        let pollDescription = args.slice(1).join(' ');
+
+        let embedPoll = new Discord.MessageEmbed()
+        .setTitle('😲 New Poll! 😲')
+        .setDescription(pollDescription)
+        .setColor('green')
+        .setFooter("React With 👍 or 👎 to answer!")
+        let msgEmbed = await pollChannel.send(embedPoll);
+        await msgEmbed.react('👍')
+        await msgEmbed.react('👎')
     }
-    let question = message.content
-      .split(`g/poll ${channel} `)
-      .join(" ");
-    if (!question)
-      return message.channel.send(`You did not specify your question!`);
-    const Embed = new Discord.MessageEmbed()
-        .setTitle(`New Poll!`)
-        .setDescription(`Question: ${question}\n `)
-        .setColor(`GREEN`)
-        .setFooter(`React With 👍 or 👎 to answer!`);
-    let msg = await client.channels.cache.get(channel.id).send(Embed);
-    await msg.react("👍");
-    await msg.react("👎");
-  },
+  
 };
