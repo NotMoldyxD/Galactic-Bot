0b32724db1ea33498c0a2d40d13f7cd356499397
NotMoldyxD <71900226+NotMoldyxD@users.noreply.github.com>
2020-09-27 13:56:52 -0400

    Update spank.js

--- commands/spank.js
+++ commands/spank.js
@@ -1,4 +1,5 @@
 const superagent = require('superagent');
+const Discord = require('discord.js');
 const customisation = require('../customisation.json');
   module.exports = {
     name: 'spank',
@@ -8,16 +9,15 @@ const customisation = require('../customisation.json');
 run: async (client, message, args, tools) => {
     if (!message.mentions.users.first()) return message.reply("You need to mention someone to spank them");
     if(!message.channel.nsfw) return message.reply("NSFW is not enabled in this channel");
-    if(message.mentions.users.first().id === "242263403001937920") return message.reply('You can\'t spank my Dev baka.:facepalm: He will spank your ass off >:3');
+    if(message.mentions.users.first().id === "326604688373973003") return message.reply('You can\'t spank my Dev baka.:facepalm: He will spank your ass off >:3');
     const { body } = await superagent
     .get("https://nekos.life/api/v2/img/spank");
     
     const embed = new Discord.MessageEmbed()
-    .setColor("#ff9900")
+    .setColor("green")
     .setTitle(`${message.mentions.users.first().username}, you got spanked in da butt by ${message.author.username} >:3`)
     .setImage(body.url) 
     .setFooter(`© Galactic Bot by ${customisation.ownername}`);
     message.channel.send({embed})
 }
 };
-
