2642d3a5d89ca5b9ae3d98bcba35cc985fdb567b
NotMoldyxD <71900226+NotMoldyxD@users.noreply.github.com>
2020-09-26 23:30:58 -0400

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,37 +1,29 @@
-const Discord = require('discord.js')
-
 module.exports = {
     name: "clear",
-    description: "Clears desired amount of messages",
-    cooldown: 10,
-    aliases: ["c", "purge", "delete"],
-    async run (message, args) {
-        const args = message.content.split(' ').slice(1);
-        const amount = args.join(' ');
-        const adminembed = new Discord.MessageEmbed()
-            .setColor('RANDOM')
-            .setTitle('Access denied')
-            .addField('Reason: ', 'this command requires message delete priveleges')
-        if (!message.member.hasPermission("DELETE_MESSAGES")) return message.channel.send(adminembed)
-        if (!amount) return message.reply('You haven\'t given an amount of messages which should be deleted!');
-        if (isNaN(amount)) return message.reply('The amount parameter isn`t a number!');
+    description: "Clears messages",
+    category: "moderation",
+    usage: "clear <ammount>",
 
-        if (amount > 100) return message.reply('You can`t delete more than 100 messages at once!');
-        if (amount < 1) return message.reply('You have to delete at least 1 message!');
-        await message.channel.messages.fetch({ limit: amount }).then(messages => {
+    async run (client, message, args) {
+
+        const amount = args.join(" ");
+
+         if(!message.member.hasPermission("ADMINISTRATOR")) {
+      return message.channel.send(`**${message.author.username}**, You do not have perms to clear messages`)
+    }
+
+        if(!amount) return message.reply('please provide an amount of messages for me to delete')
+
+        if(amount > 100) return message.reply(`you cannot clear more than 100 messages at once`)
+
+        if(amount < 1) return message.reply(`you need to delete at least one message`)
+
+        await message.channel.messages.fetch({limit: amount}).then(messages => {
             message.channel.bulkDelete(messages
-            ); setTimeout(function () {
-                const embed = new Discord.MessageEmbed()
-                    .setColor('RANDOM')
-                    .setTitle('Cleared')
-                    .addField('Messages Cleared', `\`\`\`css\n${args.join(' ')}\`\`\``)
-                message.channel.send(embed)
-                    .then(message => {
-                        message.delete({ timeout: 850 })
-                    })
-                    .catch(console.error)
-            }, 3000)
-            console.log('Clear command run')
-        })
+    )});
+
+
+    message.channel.send('Success!')
+
     }
 }
