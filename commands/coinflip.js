68c666d94b316c684644556e350dd0ecaa1346bd
NotMoldyxD <71900226+NotMoldyxD@users.noreply.github.com>
2020-09-27 14:58:45 -0400

    Update coinflip.js

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'coinflip',
     description: 'Flip a Coin',
     usage: 'coinflip',
-    category 'fun',
+    category: 'fun',
     run: async (bot, message, args) => {
     let random = (Math.floor(Math.random() * Math.floor(2)));
     if(random === 0) {
