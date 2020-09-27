48fa8db65f12f85cfb9e915c170433e05f2aa494
NotMoldyxD <71900226+NotMoldyxD@users.noreply.github.com>
2020-09-27 12:55:56 -0400

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -4,9 +4,12 @@ module.exports = {
   description: "Create a simple yes or no poll",
   category: "fun",
   run: async (client, message, args) => {
+        if(!message.member.hasPermission("ADMINISTRATOR")) {
+      return message.channel.send(`**${message.author.username}**, You do not have perms to make a poll`)
+    }
         let pollChannel = message.mentions.channels.first();
         let pollDescription = args.slice(1).join(' ');
-
+        message.channel.bulkDelete()
         let embedPoll = new Discord.MessageEmbed()
         .setTitle('😲 New Poll! 😲')
         .setDescription(`Question:${pollDescription}`)
