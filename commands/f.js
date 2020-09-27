d6f5d5a0d11dfcb5c6b5fbec4397502ba689d3f8
NotMoldyxD <71900226+NotMoldyxD@users.noreply.github.com>
2020-09-26 23:40:11 -0400

    Update f.js

--- commands/f.js
+++ commands/f.js
@@ -7,7 +7,7 @@ const customisation = require('../customisation.json');
     category: 'actions',
 run: async (client, message, args) => {
     if(args && args.length > 1){
-        message.channel.send(`${message.author.username} has paid their respect for **${args.join(' ')}** <:MafuHearty:596554617349865493>`)
+        message.channel.send(`${message.author.username} has paid their respect for **${args.join(' ')}** 💖`)
     }else{
         message.channel.send(`${message.author.username} has paid their respect <:MafuHearty:596554617349865493>`)
     }
