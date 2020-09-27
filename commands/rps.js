bfbc2bebc454fc8a13636841979485d8095df0a1
NotMoldyxD <71900226+NotMoldyxD@users.noreply.github.com>
2020-09-27 00:09:34 -0400

    Create rps.js

---
+++ commands/rps.js
@@ -0,0 +1,14 @@
+let rps = ["**:pencil: paper**", "**:moyai: rock**", "**:scissors: scissors**"];
+function random() { return `${rps[Math.floor(Math.random() * Math.floor(2))]}!` }
+module.exports = {
+    name: 'rps',
+    description: 'Rock, Papper, Scissors.',
+    usage: 'rps',
+    category: 'game',
+run: (client, msg, args) => {
+    let choice = args.join(" ").toLowerCase();
+    if (choice === '') return msg.reply("Please specify either rock, paper or scissors.");
+    if (choice !== "rock" && choice !== "paper" && choice !== "scissors") return msg.reply(`Please specify either rock, paper or scissors. ${choice} isn't one of those :P`);
+    msg.reply(random());
+}
+}
