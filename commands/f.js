a0c5470e83e53bf128c720211871b0ee2be13f5a
NotMoldyxD <71900226+NotMoldyxD@users.noreply.github.com>
2020-09-26 23:35:05 -0400

    Create f.js

---
+++ commands/f.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const customisation = require('../customisation.json');
+  module.exports = {
+    name: 'f',
+    description: 'Press F to pay Respekt',
+    usage: 'f',
+    category: 'actions',
+run: async (client, message, args) => {
+    if(args && args.length > 1){
+        message.channel.send(`${message.author.username} has paid their respect for **${args.join(' ')}** <:MafuHearty:596554617349865493>`)
+    }else{
+        message.channel.send(`${message.author.username} has paid their respect <:MafuHearty:596554617349865493>`)
+    }
+   }
+};
+ 
+  
