8c25f3571af057a598ab1ab95b045aabde9e7727
NotMoldyxD <71900226+NotMoldyxD@users.noreply.github.com>
2020-09-28 13:27:34 -0400

    Update giveaway.js

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -32,7 +32,7 @@ if(!message.member.hasPermission("ADMINISTRATOR")) {
       .setDescription(
         `The user ${message.author} is hosting a giveaway for the prize of **${prize}**`)
       .setTimestamp(Date.now() + ms(args[0]))
-      .setColor(`green`);
+      .setColor(`GREEN`);
     let m = await channel.send(Embed);
     m.react("🎉");
     setTimeout(() => {
