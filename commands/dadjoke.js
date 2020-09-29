49f99bbafe28c063c038b5ab11d8f7be0c581679
NotMoldyxD <71900226+NotMoldyxD@users.noreply.github.com>
2020-09-29 09:32:39 -0400

    Update dadjoke.js

--- commands/dadjoke.js
+++ commands/dadjoke.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "dadjoke",
     description: 'Sends a Horible dad joke that makes you cringe.',
     usage: 'dadjoke',
-    category: 'fun',
+    category: '😆**fun**😆',
 run: async (client, message, args) => {
     args = args.join(" ");
     message.channel.send(`${dadjoke[Math.floor(Math.random() * dadjoke.length)]}`);
